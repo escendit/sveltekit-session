b6b623bb16d0af42cc7118ae1968c815d5a0ffc9
Simon Novak <25969+snovak7@users.noreply.github.com>
2025-10-27 10:41:36 +0100

    refactor: clean up types and improve formatting consistency

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-import type { RequestEvent, ResolveOptions, MaybePromise } from "@sveltejs/kit";
+import type { RequestEvent, ResolveOptions } from '@sveltejs/kit';
 import type { ISessionStore } from '$lib/ISessionStore.js';
 import type { ISessionHasher } from '$lib/ISessionHasher.js';
 import type { ISessionGenerator } from '$lib/ISessionGenerator.js';
@@ -10,34 +9,35 @@ import type { ISessionGenerator } from '$lib/ISessionGenerator.js';
 type SessionConfig = {
 	cookie?: string;
 	expireIn?: number;
-    size?: number;
-    sessionStore?: ISessionStore;
-    sessionHasher?: ISessionHasher;
-    sessionGenerator?: ISessionGenerator;
-}
+	size?: number;
+	sessionStore?: ISessionStore;
+	sessionHasher?: ISessionHasher;
+	sessionGenerator?: ISessionGenerator;
+};
 
 /**
  * Internal Session configuration.
  */
 type InternalSessionConfig = {
-    cookie: string;
-    expireIn: number;
-    size: number;
-    sessionStore: ISessionStore;
-    sessionHasher: ISessionHasher;
-    sessionGenerator: ISessionGenerator;
-}
+	cookie: string;
+	expireIn: number;
+	size: number;
+	sessionStore: ISessionStore;
+	sessionHasher: ISessionHasher;
+	sessionGenerator: ISessionGenerator;
+};
+
+type MaybePromise<T> = T | Promise<T>;
 
 /**
  * Internal middleware handle.
  */
-type InternalMiddlewareHandle = (input: {
-	event: RequestEvent;
-	resolve: (event: RequestEvent, opts?: ResolveOptions) => MaybePromise<Response>;
-}, options: InternalSessionConfig) => MaybePromise<Response>;
+type InternalMiddlewareHandle = (
+	input: {
+		event: RequestEvent;
+		resolve: (event: RequestEvent, opts?: ResolveOptions) => MaybePromise<Response>;
+	},
+	options: InternalSessionConfig
+) => MaybePromise<Response>;
 
-export {
-    type SessionConfig,
-	type InternalMiddlewareHandle,
-    type InternalSessionConfig
-}
+export { type SessionConfig, type InternalMiddlewareHandle, type InternalSessionConfig };
